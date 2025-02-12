7f46239b73a51362b91d5fef17a29f9463c49fe5
aleksa <aleksa.ilic.dv@outlook.com>
2025-02-12 02:24:04 +0100

    create static UI design for app

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -4,6 +4,11 @@ const getHome = async (_req: Request, res: Response): Promise<void> => {
   res.render('home');
 };
 
+const getApp = async (_req: Request, res: Response): Promise<void> => {
+  res.render('app');
+};
+
 export default {
   getHome,
+  getApp,
 };
--- src/routes/homeRoutes.ts
+++ src/routes/homeRoutes.ts
@@ -1,9 +1,10 @@
-import express from "express";
-import homeController from "../controllers/homeController";
+import express from 'express';
+import homeController from '../controllers/homeController';
 
 const router = express.Router();
 
 // Get methods
-router.get("/", homeController.getHome);
+router.get('/', homeController.getHome);
+router.get('/app', homeController.getApp);
 
 export default router;
